fc5d3cd892d21aaada5d1adfa16c039aedcf3214
fedecalendino <fede@calendino.com>
2020-09-14 12:58:55 +0200

    Reduce sleep time

--- scraper.py
+++ scraper.py
@@ -33,5 +33,5 @@ def fetch_lyrics(url, attemps=5):
     try:
         return _fetch(url)
     except:
-        time.sleep(1)
+        time.sleep(0.1)
         return fetch_lyrics(url, attemps=attemps - 1)
